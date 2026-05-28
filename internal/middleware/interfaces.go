cc909b2b8ff8de1ca361ee2cd0b4ddcc9cc58602
tqk114514 <tqk114514@proton.me>
2026-05-29 01:16:13 +0800

    refactor: 抽取出限流器管理器接口到单独文件

---
+++ internal/middleware/interfaces.go
@@ -0,0 +1,17 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+// RateLimiterManager 限流器管理器接口
+type RateLimiterManager interface {
+	LoginRateLimit() gin.HandlerFunc
+	RegisterRateLimit() gin.HandlerFunc
+	ResetPasswordRateLimit() gin.HandlerFunc
+	OAuthTokenRateLimit() gin.HandlerFunc
+	InvalidateCodeRateLimit() gin.HandlerFunc
+	EmailAllow(email string) bool
+	EmailWaitTime(email string) int
+	DataExportAllow(userUID string) bool
+	DataExportWaitTime(userUID string) int
+	StopAll()
+}
